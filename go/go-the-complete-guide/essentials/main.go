47f76ab9a3ee104c8e3321ccf37e5637be69f2d6
juba <juliano.pedraca@gmail.com>
2024-05-18 14:12:27 -0300

    undestanding value types

--- go/go-the-complete-guide/essentials/main.go
+++ go/go-the-complete-guide/essentials/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello Wolrd!")
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
 }
