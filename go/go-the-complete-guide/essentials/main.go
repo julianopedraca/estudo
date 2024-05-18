add069e646574969076f795009dfb1124b3fb3b7
juba <juliano.pedraca@gmail.com>
2024-05-18 14:27:28 -0300

    using alternative variable declarion styles

--- go/go-the-complete-guide/essentials/main.go
+++ go/go-the-complete-guide/essentials/main.go
@@ -6,11 +6,9 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate float64 = 5.5
-	var years float64 = 10
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
-	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 
 	fmt.Println(futureValue)
 }
