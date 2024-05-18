ab8dcbe408ae76182ef5551e77246dd19dc9f12e
juba <juliano.pedraca@gmail.com>
2024-05-18 14:40:52 -0300

    uderstanding the importance of variables

--- go/go-the-complete-guide/essentials/main.go
+++ go/go-the-complete-guide/essentials/main.go
@@ -9,7 +9,9 @@ func main() {
 	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	years := 10.0
-	expectedReturnRate := 5.5
+	expectedReturnRate := 10.0
+
+	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
