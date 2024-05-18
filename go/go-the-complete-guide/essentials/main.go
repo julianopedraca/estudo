db574c82ff817bfaaa3fb019b0fba4d4e54e07cf
juba <juliano.pedraca@gmail.com>
2024-05-18 14:46:44 -0300

    improved-user-input-fetching

--- go/go-the-complete-guide/essentials/main.go
+++ go/go-the-complete-guide/essentials/main.go
@@ -7,11 +7,18 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	years := 10.0
-	expectedReturnRate := 10.0
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
+	fmt.Print("Ivestment Amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
