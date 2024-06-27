375c1edbf8e9ceb03395355b63c49d4b148177a5
juba <juliano.pedraca@gmail.com>
2024-06-26 22:47:56 -0300

    goroutines and channels

---
+++ go/go-the-complete-guide/practice-project-price-calculator/prices/prices.go
@@ -0,0 +1,69 @@
+package prices
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"example.com/price-calculator/conversion"
+)
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxincludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	prices, err := conversion.StringsToFloat(lines)
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Reading the file content failed")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	job.InputPrices = prices
+	file.Close()
+}
+
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadData()
+
+	result := make(map[string]string)
+
+	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
+	}
+	fmt.Println(result)
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}
